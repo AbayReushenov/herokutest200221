5caab8f52e4194c44375d15a833f494a193ed3bf
Abay <abay.reushenov@gmail.com>
2021-02-21 13:24:42 +0300

    Commit before HEROKU uploid

--- data/database.js
+++ data/database.js
@@ -13,12 +13,21 @@ const options = {
   // family: 4 // Use IPv4, skip trying IPv6
 };
 
-const dbURL = `${process.env.DATABASE_CONNECTION_ADRESS}/herokutest200221`;
+const dbURL = `${process.env.DB_PREFIX}${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_CONNECTION_ADRESS}/herokutest200221?${process.env.DB_CONNECTION_OPTIONS}`;
+// mongodb+srv://databaseAdminAbay:<password>@cluster0.ndxpv.mongodb.net/herokutest200221?retryWrites=true&w=majority
+
 function dbConnect() {
   mongoose.connect(dbURL, options, (error) => {
     if (error) return console.log(error);
     return console.log(dbURL, ' ready to use.');
   });
+
+  // const dbURL = `${process.env.DB_CONNECTION_ADRESS}/herokutest200221`;
+  // function dbConnect() {
+  //   mongoose.connect(dbURL, options, (error) => {
+  //     if (error) return console.log(error);
+  //     return console.log(dbURL, ' ready to use.');
+  //   });
 }
 
 module.exports = dbConnect;
